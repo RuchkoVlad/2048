9d5bb124cf5f6dd9bbee515a24c2b1465712c6c5
Vlad Ruchko <necessarydp@gmail.com>
2019-05-26 00:44:53 +0300

    fixed appearance

--- control_keys.js
+++ control_keys.js
@@ -1,24 +1,24 @@
 'use strict';
 
-document.addEventListener("keyup", function (event) {
+document.addEventListener('keyup', function (event) {
     if (event.keyCode === 39) {
         moveRight();
     }
 });
 
-document.addEventListener("keyup", function (event) {
+document.addEventListener('keyup', function (event) {
     if (event.keyCode === 37) {
         moveLeft();
     }
 });
 
-document.addEventListener("keyup", function (event) {
+document.addEventListener('keyup', function (event) {
     if (event.keyCode === 38) {
         moveTop();
     }
 });
 
-document.addEventListener("keyup", function (event) {
+document.addEventListener('keyup', function (event) {
     if (event.keyCode === 40) {
         moveDown();
     }
